acf587b2eaaab1548f1d77f053bc69ebf1c0a0f8
jgmbl <joannagembel@gmail.com>
2024-04-03 11:30:23 +0200

    IFUIP-6 add import window

--- image_filtering/src/main/java/pl/jgmbl/image_filtering/ImportController.java
+++ image_filtering/src/main/java/pl/jgmbl/image_filtering/ImportController.java
@@ -1,4 +1,14 @@
 package pl.jgmbl.image_filtering;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ImportController {
+    @FXML
+    private Label importData;
+
+    @FXML
+    protected void onImportButtonClick() {
+        importData.setText("Importing images...");
+    }
 }
