e6bddd919449a9193785f74b2e16509c39c9318a
jgmbl <joannagembel@gmail.com>
2024-04-04 18:34:21 +0200

    IFUIP-9 add listOfFiles method

--- image_filtering/src/main/java/pl/jgmbl/image_filtering/ImportService.java
+++ image_filtering/src/main/java/pl/jgmbl/image_filtering/ImportService.java
@@ -1,14 +1,26 @@
 package pl.jgmbl.image_filtering;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImportService {
-    @FXML
-    private Label exportImages;
 
-    @FXML
-    protected void onImportButtonClick() {
-        exportImages.setText("Import images...");
+    public List<String> listOfFiles(String path) {
+        ArrayList<String> filesList = new ArrayList<>();
+
+        File folder = new File(path);
+
+        if (folder.isDirectory()) {
+            File[] files = folder.listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    filesList.add(file.getName());
+                }
+            }
+        }
+        return filesList;
     }
+
 }
