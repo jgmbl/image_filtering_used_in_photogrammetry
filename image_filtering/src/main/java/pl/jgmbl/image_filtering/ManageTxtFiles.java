8a3f73aa94908196f655c1127b5ee4bc418523ee
jgmbl <joannagembel@gmail.com>
2024-04-08 10:46:51 +0200

    IFUIP-11 add write method to ManageTxtFiles

--- image_filtering/src/main/java/pl/jgmbl/image_filtering/ManageTxtFiles.java
+++ image_filtering/src/main/java/pl/jgmbl/image_filtering/ManageTxtFiles.java
@@ -1,4 +1,20 @@
 package pl.jgmbl.image_filtering;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+
 public class ManageTxtFiles {
+    public ManageTxtFiles() {
+    }
+
+    public void writeListToTxtFile(List<String> list) throws IOException {
+        Path path = Paths.get("src/main/resources/images.txt");
+
+        Files.write(path, list, StandardOpenOption.APPEND);
+    }
 }
