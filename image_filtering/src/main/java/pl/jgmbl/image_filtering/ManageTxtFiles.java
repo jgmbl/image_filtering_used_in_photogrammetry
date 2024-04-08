9f0c7067af36a07cbdb7f0f1981c49f34171b307
jgmbl <joannagembel@gmail.com>
2024-04-08 11:08:14 +0200

    IFUIP-11 add checking path method

--- image_filtering/src/main/java/pl/jgmbl/image_filtering/ManageTxtFiles.java
+++ image_filtering/src/main/java/pl/jgmbl/image_filtering/ManageTxtFiles.java
@@ -16,11 +16,25 @@ public class ManageTxtFiles {
     public void writeListToTxtFile(List<String> list) throws IOException {
         Path path = Paths.get("src/main/resources/images.txt");
 
+        if (!Files.exists(path)) {
+            createTxtFile(path);
+        }
+
         //appending first line of data in separate line
         ArrayList<String> listOfImages = new ArrayList<>();
         listOfImages.add(System.lineSeparator());
         listOfImages.addAll(list);
 
         Files.write(path, listOfImages, StandardOpenOption.APPEND);
+    }
+
+    public void createTxtFile(Path path) {
+        if (!Files.exists(path)) {
+            try {
+                Files.createFile(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
