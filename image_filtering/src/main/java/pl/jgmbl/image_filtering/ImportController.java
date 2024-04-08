80c28f15b2bf4195014bfcfa944835c706aa2bb8
jgmbl <joannagembel@gmail.com>
2024-04-08 11:34:58 +0200

    IFUIP-10 add alerts

--- image_filtering/src/main/java/pl/jgmbl/image_filtering/ImportController.java
+++ image_filtering/src/main/java/pl/jgmbl/image_filtering/ImportController.java
@@ -3,6 +3,10 @@ package pl.jgmbl.image_filtering;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class ImportController {
@@ -11,8 +15,7 @@ public class ImportController {
 
     private final ImportService importService = new ImportService();
 
-    public ImportController() {
-    }
+    private final ManageTxtFiles manageTxtFiles = new ManageTxtFiles();
 
     @FXML
     protected void onImportClick() {
@@ -20,6 +23,21 @@ public class ImportController {
 
         List<String> listOfJPGFiles = importService.listOfJPGFiles(importDirectory);
 
-        AddAlert.addInfoAlert("Import succeed", "Choose filtering option from main menu");
+        try {
+            if (checkIfPathExists(importDirectory)) {
+                manageTxtFiles.writeListToTxtFile(listOfJPGFiles);
+                AddAlert.addInfoAlert("Import succeed", "Choose filtering option from main menu");
+            } else {
+                AddAlert.addErrorAlert("Import failed", "Check if path is correct");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean checkIfPathExists(String path) {
+        Path path1 = Paths.get(path);
+
+        return Files.exists(path1);
     }
 }
