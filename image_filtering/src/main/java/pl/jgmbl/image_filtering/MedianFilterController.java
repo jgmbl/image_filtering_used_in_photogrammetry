379df0e2323a71fb95219d7cae97e105c8c54ade
jgmbl <joannagembel@gmail.com>
2024-04-12 11:21:31 +0200

    IFUIP-22 refactor the code

--- image_filtering/src/main/java/pl/jgmbl/image_filtering/MedianFilterController.java
+++ image_filtering/src/main/java/pl/jgmbl/image_filtering/MedianFilterController.java
@@ -1,20 +1,15 @@
 package pl.jgmbl.image_filtering;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
 
-public class MedianFilterController {
-    String IMAGES_TXT_PATH = "src/main/resources/images.txt";
+public class MedianFilterController extends IndexFilterController {
+    public final String typeOfFiltering = "median";
 
     @FXML
     private Label filterInfo;
@@ -34,9 +29,7 @@ public class MedianFilterController {
     private Label sampleImageInfo;
     @FXML
     private ImageView sampleImage;
-
-    private final ProcessImagesService processFiles = new ProcessImagesService();
-
+    
 
     public void initialize() {
         filterInfo.setText("Median filtering is used to reduce noise while smoothing and preserving edge sharpness." +
@@ -50,40 +43,21 @@ public class MedianFilterController {
     }
 
 
-    public void onExportClick() throws IOException {
+    @FXML
+    @Override
+    public void onExportClick() {
         String exportPath = this.exportPath.getText();
         String blurringParameterString = blurringParameter.getText();
-
-        exportPath = InputValidationService.returnCorrectPath(exportPath);
-
-        if (!InputValidationService.checkIfExportPathIsCorrect(exportPath)) {
-            AddAlert.addErrorAlert("Export failed", "Check if the folder path is correct.");
-            return;
-        }
-
-        if (!InputValidationService.checkIfParameterIsCorrect(blurringParameterString)) {
-            AddAlert.addErrorAlert("Export failed", "Check if the blur parameter is correct.");
-            return;
-        }
-
-        int blurringParameterValue = Integer.parseInt(blurringParameterString);
-
-        processFiles.filtering("median", IMAGES_TXT_PATH, exportPath, blurringParameterValue);
-
-        List<String> listOfBlurredImages = processFiles.listOfFilteredImages(exportPath, "median_");
-        ObservableList<String> blurredImagesObservableList = FXCollections.observableArrayList(listOfBlurredImages);
-
-        exportData.setText("List of filtered images in export folder: ");
-        exportedImagesList.setItems(blurredImagesObservableList);
 
         this.exportPath.clear();
         blurringParameter.clear();
 
-        AddAlert.addInfoAlert("Export succeed", "Filtered images are saved. If you want to change the level of blur, just do the filtering again.");
-
-        FileInputStream input = new FileInputStream(processFiles.returnFirstFilteredImage(exportPath, "median_"));
-        Image image = new Image(input);
-        sampleImage.setImage(image);
+        try {
+            exportImages(exportPath, blurringParameterString, typeOfFiltering);
+            setUI(exportData, exportedImagesList, exportPath, sampleImage, typeOfFiltering);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
