360c9b8e20e7252eaf742fbcfb8cc3c6b93540ac
jgmbl <joannagembel@gmail.com>
2024-04-12 11:45:51 +0200

    IFUIP-4 add FXML fields

--- image_filtering/src/main/java/pl/jgmbl/image_filtering/SharpeningFilterController.java
+++ image_filtering/src/main/java/pl/jgmbl/image_filtering/SharpeningFilterController.java
@@ -1,4 +1,31 @@
 package pl.jgmbl.image_filtering;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class SharpeningFilterController {
+    private final String typeOfFiltering = "sharpening";
+    private final String blurringParameter = "1";
+
+    @FXML
+    private Label filterInfo;
+
+    @FXML
+    private Label exportInfo;
+    @FXML
+    private TextField exportPath;
+    @FXML
+    private Label exportData;
+    @FXML
+    private ListView<String> exportedImagesList;
+
+    @FXML
+    private Label sampleImageInfo;
+    @FXML
+    private ImageView sampleImage;
+
+
 }
