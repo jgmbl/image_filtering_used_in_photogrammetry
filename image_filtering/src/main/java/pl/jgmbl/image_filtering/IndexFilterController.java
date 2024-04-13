549a0b4f4e43b394c5841cca8739b6b63d3f3bb0
jgmbl <joannagembel@gmail.com>
2024-04-13 19:06:06 +0200

    IFUIP-4 save changes

--- image_filtering/src/main/java/pl/jgmbl/image_filtering/IndexFilterController.java
+++ image_filtering/src/main/java/pl/jgmbl/image_filtering/IndexFilterController.java
@@ -9,7 +9,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
