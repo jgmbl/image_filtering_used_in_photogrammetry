1aa550a74f9b6324f3c94b65bbc0b55eedf13647
jgmbl <joannagembel@gmail.com>
2024-04-13 22:03:27 +0200

    IFUIP-29 add ImageFilteringApplicationTest

---
+++ image_filtering/src/test/java/pl/jgmbl/image_filtering/ImageFilteringApplicationTest.java
@@ -0,0 +1,20 @@
+package pl.jgmbl.image_filtering;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageFilteringApplicationTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void stop() {
+    }
+
+    @Test
+    void main() {
+    }
+}
