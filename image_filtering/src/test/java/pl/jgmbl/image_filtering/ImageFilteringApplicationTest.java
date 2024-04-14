45fc29bf3e241f38da6cc902f02b431dd22d7f34
jgmbl <joannagembel@gmail.com>
2024-04-14 11:13:19 +0200

    IFUIP-29 add setUp and tearDown method

--- image_filtering/src/test/java/pl/jgmbl/image_filtering/ImageFilteringApplicationTest.java
+++ image_filtering/src/test/java/pl/jgmbl/image_filtering/ImageFilteringApplicationTest.java
@@ -1,10 +1,47 @@
 package pl.jgmbl.image_filtering;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
 
 class ImageFilteringApplicationTest {
+    private ImageFilteringApplication imageFilteringApplication;
+
+    @Before
+    public void setUp() throws IOException {
+        imageFilteringApplication = new ImageFilteringApplication();
+        HashSet<String> setOfImagesPaths = new HashSet<>();
+        setOfImagesPaths.add("src/test/resources/1.1.12.tiff");
+        setOfImagesPaths.add("src/test/resources/4.1.05.jpg");
+        setOfImagesPaths.add("src/test/resources/4.2.05.tiff");
+        setOfImagesPaths.add("src/test/resources/5.1.13.jpg");
+
+        Path path = Paths.get("src/test/resources/test_images.txt");
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+
+            if (Files.size(path) == 0) {
+                Files.write(path, setOfImagesPaths, StandardOpenOption.TRUNCATE_EXISTING);
+            }
+        }
+    }
+
+    @After
+    public void tearDown() {
+        File file = new File("src/test/resources/test_images.txt");
+
+        if (file.exists()) {
+            file.delete();
+        }
+    }
 
     @Test
     void start() {
